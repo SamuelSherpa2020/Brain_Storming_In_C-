bf6e5ca0e6822ea2e26dff92641ccce3a513d241
SamuelSherpa2020 <samuelsherpa46@gmail.com>
2023-02-05 21:34:11 +0545

    Add Alphabet Pyramid

---
+++ TriangleAlpha/EntryPoint.cs
@@ -0,0 +1,34 @@
+﻿namespace TriangleAlpha;
+class EntryPoint
+{
+    static void Main(string[] args)
+    {
+        string[] words = { "A", "B", "C", "D", "E","F"};
+
+        int l = words.Length;
+
+        char d = 'y';
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                if (d == 'y')
+                {
+                    Console.Write(new string(' ', l));
+                }
+                Console.Write(words[j]);
+                d = 'n';
+            }
+            for (int k = i; k >= 1; k--)
+            {
+                Console.Write(words[k - 1]);
+            }
+            Console.WriteLine();
+            l--;
+            d = 'y';
+
+        }
+
+    }
+}
